abde3253ff2334b9c30f51d965d16dea28b570f0
soekarsoedirman <sabiqalhimam3@gmail.com>
2025-12-07 21:37:21 +0700

    Update user.js

---
+++ src/handler/user.js
@@ -0,0 +1,49 @@
+// usrdashboard,
+// searchproduct,
+// productdetail,
+// keranjang,
+// tambahkeranjang,
+// detailkeranjang,
+// hapuskeranjang,
+// order,
+// orderlist,
+// orderdetail,
+// orderconfirm,
+// ordercancel
+
+const usrdashboard = async (request, h) => {
+
+};
+const searchproduct = async (request, h) => {
+
+};
+const productdetail = async (request, h) => {
+
+};
+const keranjang = async (request, h) => {
+
+};
+const tambahkeranjang = async (request, h) => {
+
+};
+const detailkeranjang = async (request, h) => {
+
+};
+const hapuskeranjang = async (request, h) => {
+
+};
+const order = async (request, h) => {
+
+};
+const orderlist = async (request, h) => {
+
+};
+const orderdetail = async (request, h) => {
+
+};
+const orderconfirm = async (request, h) => {
+
+};
+const ordercancel = async (request, h) => {
+
+};
